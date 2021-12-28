3dec78934f97de83f805f89eadcbd740666effe0
Shubh <patelshubh694@gmail.com>
2021-12-28 15:28:00 +0530

    Installation - NextJs, TailwindCSS

---
+++ spotify-2.0/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>This is a Spotify 2.0 Build</h1>
+
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+
+      {/* Player */}
+    </div>
+  )
+}
