2559d51e07f4f0b3da54e616dfa8e83d2603a2c0
Shubh <patelshubh694@gmail.com>
2021-12-29 16:15:05 +0530

    Render the Song & Artist Names with Song Duration and Designed that section

--- spotify-2.0/components/Center.js
+++ spotify-2.0/components/Center.js
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import {shuffle} from "lodash";
 import { useRecoilState, useRecoilValue } from "recoil";
@@ -9,7 +9,7 @@ import Songs from "./Songs";
 
 const colors = [
     "from-indigo-500",
-    "from-bluw-500",
+    "from-blue-500",
     "from-green-500",
     "from-red-500",
     "from-yellow-500",
@@ -34,12 +34,10 @@ const Center = () => {
         }).catch((err) => console.log("Something went wrong!", err));
     }, [spotifyApi, playlistId]);
 
-    console.log(playlist);
-
     return (
-        <div className="flex-grow">
+        <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
             <header className="absolute top-5 right-8">
-                <div className="text-white flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+                <div onClick={() => signOut()} className="text-white flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
                     <img className="rounded-full w-10 h-10" src={session?.user?.image} alt="" />
                     <h2>{session?.user?.name}</h2>
                     <ChevronDownIcon className="h-5 w-5" />
--- spotify-2.0/components/Songs.js
+++ spotify-2.0/components/Songs.js
@@ -1,7 +1,15 @@
+import { useRecoilValue } from "recoil";
+import { playlistState } from "../atoms/playlistAtom";
+import Song from "./Song";
+
 const Songs = () => {
+    const playlist = useRecoilValue(playlistState);
+
     return (
-        <div>
-            
+        <div className="text-white px-8 flex flex-col space-y-1 pb-28">
+            {playlist?.tracks.items.map((track, i) => (
+                <Song key={track.track.id} track={track} order={i} />
+            ))}
         </div>
     )
 }
